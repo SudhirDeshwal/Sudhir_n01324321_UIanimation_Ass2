a079f3012c2c9d0755875f09e199f63db9907049
Sudhir Deshwal <DES>
2020-07-28 07:20:11 -0400

    new screens created

--- Sudhir_n01324321_UIanimation_Ass2/SecondAniViewController.swift
+++ Sudhir_n01324321_UIanimation_Ass2/SecondAniViewController.swift
@@ -9,22 +9,47 @@
 import UIKit
 
 class SecondAniViewController: UIViewController {
+    
+    
+    //my image
+    @IBOutlet weak var myImage: UIImageView!
+    
+    //
+    let newButtonWidth: CGFloat = 100
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+            let oldValue = myImage.frame.width/2
+
+/* Do Animations */
+CATransaction.begin() //1
+CATransaction.setAnimationDuration(2.0) //2
+CATransaction.setAnimationTimingFunction(CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)) //3
+
+// View animations //4
+        UIView.animate(withDuration: 0.5) {
+    self.myImage.frame = CGRect(x: 0, y: 0, width: self.newButtonWidth, height: self.newButtonWidth)
+    self.myImage.center = self.view.center
+}
+
+// Layer animations
+let cornerAnimation = CABasicAnimation(keyPath: #keyPath(CALayer.cornerRadius)) //5
+cornerAnimation.fromValue = oldValue //6
+cornerAnimation.toValue = newButtonWidth/2 //7
+
+myImage.layer.cornerRadius = newButtonWidth/2 //8
+myImage.layer.add(cornerAnimation, forKey: #keyPath(CALayer.cornerRadius)) //9
+
+CATransaction.commit() //10
+        
+        
+        
+        
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
--- Sudhir_n01324321_UIanimation_Ass2/ThirdAniViewController.swift
+++ Sudhir_n01324321_UIanimation_Ass2/ThirdAniViewController.swift
@@ -9,22 +9,46 @@
 import UIKit
 
 class ThirdAniViewController: UIViewController {
+    
+    //using ui kit dynamics
+     var dynamicAnimator   : UIDynamicAnimator!
+   var gravityBehavior   : UIGravityBehavior!
+    
+    var collisionBehavior : UICollisionBehavior!
+    var bouncingBehavior  : UIDynamicItemBehavior!
+    
 
+    //my Image
+    @IBOutlet weak var myImgview: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+      
+
+       dynamicAnimator = UIDynamicAnimator(referenceView: self.view) //1
+
+       gravityBehavior = UIGravityBehavior(items: [myImgview]) //2
+       dynamicAnimator.addBehavior(gravityBehavior) //3
+        
+        
+        
+        collisionBehavior = UICollisionBehavior(items: [myImgview]) //4
+        collisionBehavior.translatesReferenceBoundsIntoBoundary = true //5
+        dynamicAnimator.addBehavior(collisionBehavior) //6
+        
+        
+        //Adding the bounce effect
+        bouncingBehavior = UIDynamicItemBehavior(items: [myImgview]) //7
+        bouncingBehavior.elasticity = 0.75 //8
+        dynamicAnimator.addBehavior(bouncingBehavior) //9
+        
+        
+        
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
